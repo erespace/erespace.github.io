9e24378cd239ce0b9589855414eaba6db2204806
erespace <erespace@gmail.com>
2016-09-01 15:02:07 +0100

    Update somethingviral.js

--- somethingviral.js
+++ somethingviral.js
@@ -1,12 +1,12 @@
 
 // ucs-2 string to base64 encoded ascii
-function utoa(str) {
-    return window.btoa(unescape(encodeURIComponent(str)));
-}
+//function utoa(str) {
+ //   return window.btoa(unescape(encodeURIComponent(str)));
+//}
 // base64 encoded ascii to ucs-2 string
-function atou(str) {
-    return decodeURIComponent(escape(window.atob(str)));
-}
+//function atou(str) {
+//    return decodeURIComponent(escape(window.atob(str)));
+//}
 
 
 
