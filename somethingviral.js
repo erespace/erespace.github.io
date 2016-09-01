140b57915bae8ce8fa28548c9866931d8afe6efc
erespace <erespace@gmail.com>
2016-09-01 14:58:29 +0100

    Update somethingviral.js

--- somethingviral.js
+++ somethingviral.js
@@ -27,9 +27,10 @@ function atou(str) {
 	
 	 function create(){
 	 var redirect_path = window.location.pathname;
-	 var product = LZString.compress(document.getElementById("new_viral").value);
-	var product = document.getElementById("new_viral").value;
-	
+	 var product = window.btoa(document.getElementById("new_viral").value);
+	 //var product = LZString.compress(document.getElementById("new_viral").value);
+	//var product = document.getElementById("new_viral").value;
+	var product = LZString.compress(product);
 	//window.location=redirect_path+"?"+utoa(product);
 	 window.location=redirect_path+"?"+window.btoa(product);
 	 
@@ -46,8 +47,8 @@ function atou(str) {
 	 }else{
 	 var subject = window.atob(decodeURIComponent(subject));
 	
-	 //var subject = atou(decodeURIComponent(subject));	
-	// var subject = LZString.decompress(subject)	
+	 var subject = atou(decodeURIComponent(subject));	
+	 var subject = LZString.decompress(subject)	
 	 }
 	 document.getElementById("subject").innerHTML=subject;
 	 document.getElementById("subjectimage").src=subject;
