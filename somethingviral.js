a6b5a5fc21389b96210fadc27170779ea116faf7
erespace <erespace@gmail.com>
2016-09-10 13:16:51 +0100

    Update somethingviral.js

--- somethingviral.js
+++ somethingviral.js
@@ -107,7 +107,22 @@ document.getElementById("insync").src = "http://erespace.p4o.net/put.php?key="+s
 	 }
 	 //store_value("counterviralcount", counter);
 	 
-
+alert (redirect_path)
+/**
+ *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+ *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables */
+/*
+var disqus_config = function () {
+    this.page.url = redirect_path+"/"+subjectmatter;  // Replace PAGE_URL with your page's canonical URL variable
+    this.page.identifier = window.btoa(subjectmatter); // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+};
+*/
+(function() { // DON'T EDIT BELOW THIS LINE
+    var d = document, s = d.createElement('script');
+    s.src = '//http-somethingviral-tk-1.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+})();
 
 
 	 }
