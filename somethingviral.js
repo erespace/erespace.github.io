4e3e20188334fe802f2b1065e5dba8a1a38f851f
erespace <erespace@gmail.com>
2016-09-10 13:19:52 +0100

    Update somethingviral.js

--- somethingviral.js
+++ somethingviral.js
@@ -107,7 +107,7 @@ document.getElementById("insync").src = "http://erespace.p4o.net/put.php?key="+s
 	 }
 	 //store_value("counterviralcount", counter);
 	 
-alert (window.location.pathname);
+//alert (window.location.pathname);
 /**
  *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
  *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables */
