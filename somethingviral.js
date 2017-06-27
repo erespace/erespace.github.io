555596a9cb81d0207e47cfd706aced9013d66218
erespace <erespace@gmail.com>
2017-06-27 10:57:39 +0100

    Update somethingviral.js

--- somethingviral.js
+++ somethingviral.js
@@ -84,7 +84,7 @@ document.getElementById("insync").src = "http://erespace.p4o.net/put.php?key="+s
 //]]>
 //FB.XFBML.parse();	 
 		 
-		 alert (window.btoa(product));
+		 //alert (window.btoa(product));
  document.getElementById("facebookbutton1").src = "http://www.facebook.com/plugins/like.php?href=http://somethingviral.tk/one.html?"+window.btoa(product+"***a")+"&A=A&amp;layout=button_count&amp;show_faces=false&amp;width=240&amp;action=like&amp;font=verdana&amp;colorscheme=light";	 
  // var fbz = document.createElement('fb:like'); 
  //fbz.setAttribute("site","two.html"+subjectmatter);  
