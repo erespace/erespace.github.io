726cd1e16cf7b9439e61d0258d0a67d13c81b62a
erespace <erespace@gmail.com>
2017-07-12 15:01:55 +0100

    Update somethingviral.js

--- somethingviral.js
+++ somethingviral.js
@@ -90,7 +90,7 @@ document.getElementById("insync").src = "http://erespace.p4o.net/put.php?key="+s
 
 		 //alert (window.btoa(product));
 		 
-		   document.getElementById("googlebutton1").href= "http://somethingviral.tk/one.html?"+window.btoa(product+"***a")+"&A=A";
+		   ///document.getElementById("googlebutton1").href= "http://somethingviral.tk/one.html?"+window.btoa(product+"***a")+"&A=A";
  document.getElementById("facebookbutton1").src = "http://www.facebook.com/plugins/like.php?href=http://somethingviral.tk/one.html?"+window.btoa(product+"***a")+"&A=A&amp;layout=button_count&amp;show_faces=false&amp;width=240&amp;action=like&amp;font=verdana&amp;colorscheme=light";	 
 
 		 //document.getElementById("googlebutton1").href="http://somethingviral.tk/one.html?"+window.btoa(product+"***a")+"&A=A";
@@ -102,7 +102,7 @@ document.getElementById("insync").src = "http://erespace.p4o.net/put.php?key="+s
  //fbz.setAttribute("width","100");
  //fbz.setAttribute("font","arial");
  //document.getElementById("FaceBookLikeButton2").appendChild(fbz);
- document.getElementById("googlebutton2").href= "=http://somethingviral.tk/two.html?"+window.btoa(product+"***b")+"&B=B";
+// document.getElementById("googlebutton2").href= "=http://somethingviral.tk/two.html?"+window.btoa(product+"***b")+"&B=B";
 document.getElementById("facebookbutton2").src = "http://www.facebook.com/plugins/like.php?href=http://somethingviral.tk/two.html?"+window.btoa(product+"***b")+"&B=B&amp;layout=button_count&amp;show_faces=false&amp;width=240&amp;action=like&amp;font=verdana&amp;colorscheme=light";	 
 		 //document.getElementById("googlebutton2").href="http://somethingviral.tk/two.html?"+window.btoa(product+"***b")+"&B=B";
 //FB.XFBML.parse();	 
