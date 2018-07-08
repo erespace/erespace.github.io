d4d4467eeaf58668dc54fad0a601e4011a6ae3b6
erespace <erespace@gmail.com>
2018-07-08 12:17:52 +0100

    Create work.js

---
+++ work.js
@@ -0,0 +1,17 @@
+var i = 0;
+
+function timedCount() {
+    i = i + 1;
+    postMessage(i);
+    
+    
+    if(i=300){
+    
+    window.navigator.vibrate([100,30,100,30,100,30,200,30,200,30,200,30,100,30,100,30,100]);
+    
+    }
+    
+    setTimeout("timedCount()",500);
+}
+
+timedCount(); 
